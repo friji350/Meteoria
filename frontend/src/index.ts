a7b238c3ab36d6f98f22ec6763f40e5fc8885bb8
daniy <daniyatretyakov@gmail.com>
2023-07-12 21:39:27 +0300

    Backend integration

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -5,19 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
                 const headers = new Headers();
-                headers.append('X-Req-Id', '1689180792496067-16091216986806160979-jmvzo64mxhwflejj-BAL');
-                // Запрос к API погоды
 
-                const apiUrl = `https://api.weather.yandex.ru/v2/forecast?lat=${latitude}&lon=${longitude}&extra=true`;
+
+                const apiUrl = `http://127.0.0.1:8000/api/weather?lat=${latitude}&lon=${longitude}`;
 
                 fetch(apiUrl,  { headers })
                     .then((response) => response.json())
                     .then((data) => {
-                        // Обработка данных погоды
                         const city = data.geo_object.locality.name;
                         const temperature = data.fact.temp;
-                        console.log(data)
-                        // Вывод данных на страницу
                         document.body.innerHTML = `
               <h2>Погода в ${city}</h2>
               <p>Температура: ${temperature}°C</p>
